361ceb8ad1b353c04946122655d11757d0e24210
Steven Gentile <steven.gentile@gmail.com>
2018-04-02 17:22:13 -0400

    fix aot errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,5 +32,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 })
 export class AppModule { }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
 
