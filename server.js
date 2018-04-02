4562f34b0d5a06fd9c7cdc4b08d1cfd9ed8a2765
Steven Gentile <steven.gentile@gmail.com>
2018-04-02 17:09:17 -0400

    use compression

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
+const compression = require('compression');
 const path = require('path');
+
+app.use(compression());
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
